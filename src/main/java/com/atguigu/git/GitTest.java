07a3dd8a4fdc42461bab4af5a61f4c7d8335440d
nfx <nfx@atguigu.com>
2022-08-06 17:42:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author NXIONG
+ * @create 2022-08-06 17:14
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}
